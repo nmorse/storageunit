edb64e686541587da409559bb3460d6236dfaa29
Nate Morse <morse.nate@gmail.com>
2012-12-29 20:05:30 -0500

    Update storageunit.js

--- storageunit.js
+++ storageunit.js
@@ -7,14 +7,21 @@
     // if local storage is up, first merge with that.
     if (degree >= 1 && typeof(Storage) !== "undefined") {
       if (data_envelope.id) {
-        localStorage.setItem(data_envelope.id, {checks:data_envelope.checks, contents:data_envelope.contents});
+        data_envelope.checks.local_sync = Date.parse(Date());
+        localStorage.setItem(data_envelope.id, JSON.stringify({"checks":data_envelope.checks, "contents":data_envelope.contents}));
       }
       else {
         
       }
     }
-    if (degree >= 2 && typeof(Storage) !== "undefined") {
-      
+    if (degree >= 2 && navigator.onLine) {
+      if (data_envelope.id) {
+        data_envelope.checks.remote_sync = Date.parse(Date());
+        $.ajax(..., data_envelope.id, JSON.stringify({"checks":data_envelope.checks, "contents":data_envelope.contents}));
+      }
+      else {
+        
+      }      
     }
 
   };
