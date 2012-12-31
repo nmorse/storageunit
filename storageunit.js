ced6760703c9961f93552b5f93b3a1d090f95fe4
Nate Morse <morse.nate@gmail.com>
2012-12-31 12:22:35 -0500

    Update storageunit.js

--- storageunit.js
+++ storageunit.js
@@ -1,25 +1,36 @@
 // data_envelope = {id:"123", checks:{}, contents:{}}
 
 (function($) {
-  storageunit = {};
+  var opt = {};
+  var status = [];
+  storageunit = {"remote_storage_service":{"dataType": "jsonp"}, "default_degree":2, "instance_id":0};
+  storageunit.setup = function (options) {
+    opt = $.extend(opt, options);
+    if (typeof(Storage) !== "undefined")) {
+      status[1] = {};
+      status[1].has_su_data = (localStorage.getItem("storageunit_"+opt.instance_id));
+  };
   storageunit.store = function (data_envelope, degree) {
-    var previousitem;
+    
+    var requested_degree = degree;
+    if (typeof degree === "undefined") {requested_degree = opt.default_degree;}
+    
     // if local storage is up, first merge with that.
     if (degree >= 1 && typeof(Storage) !== "undefined") {
       if (data_envelope.id) {
-        data_envelope.checks.local_sync = Date.parse(Date());
         localStorage.setItem(data_envelope.id, JSON.stringify({"checks":data_envelope.checks, "contents":data_envelope.contents}));
+        data_envelope.sync.degree = Date.parse(Date());
       }
       else {
         
       }
     }
-    if (degree >= 2 && navigator.onLine) {
+    if (degree >= 2 && navigator.onLine && opt.remote_storage_service.url) {
       if (data_envelope.id) {
         data_envelope.checks.remote_start = Date.parse(Date());
         $.ajax({
-          "url": "http://service.recorder.com:9080",
-          "dataType": "jsonp",
+          "url": opt.remote_storage_service.url,
+          "dataType": opt.remote_storage_service.dataType,
           "data": data_envelope,
           "type": "GET",
           "error": function(XMLHttpRequest, textStatus, errorThrown) {
