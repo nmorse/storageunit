47e0fcf05b138c1f6e83fade4bf62cece70cf34d
Nate Morse <morse.nate@gmail.com>
2014-02-18 12:58:46 -0500

    outcome message indexing
    outcome message description are easier to maintain, with usable names

--- localstorage.js
+++ localstorage.js
@@ -1,5 +1,5 @@
 (funciton ($) {
-  var outcome = ["storage not availible", "saved to existing", "did not overwrite existing", "created new", "storage operation submitted"];
+  var outcome = {"no_storage":"storage not availible", "overwritten":"saved to existing", "not_overwritten":"did not overwrite existing", "new":"created new", "pending":"storage operation submitted"};
   var opt = {proposed_name:"new", overwrite:false};
   if (!storageunit) {storageunit = {};}
   storageunit.localstorage = {};
@@ -17,7 +17,7 @@
             if (opt.overwrite) {
                 local_hb_graphs[opt.proposed_name] = JSON.parse(export_graph_json(g));
                 localStorage.hb_graphs = JSON.stringify(local_hb_graphs);
-                $(document).trigger("hbg_save_status", [{"outcome": outcome[1], "target": "local", "final":true}]);
+                $(document).trigger("hbg_save_status", [{"outcome": outcome.overwritten, "target": "local", "final":true}]);
                 $('#graph_storage').html("local");
                 $('#graph_title').html(opt.proposed_name);
                 g_aux.name = opt.proposed_name;
@@ -25,13 +25,13 @@
                 ///$('#graph_input_name_n2').val(opt.proposed_name);
             }
             else {
-                $(document).trigger("hbg_save_status", [{"outcome": outcome[2], "target": "local", "final":true}]);
+                $(document).trigger("hbg_save_status", [{"outcome": outcome.not_overwritten, "target": "local", "final":true}]);
             }
         }
         else {
             local_hb_graphs[opt.proposed_name] = JSON.parse(export_graph_json(g));
             localStorage.hb_graphs = JSON.stringify(local_hb_graphs);
-            $(document).trigger("hbg_save_status", [{"outcome": outcome[3], "target": "local", "final":true}]);
+            $(document).trigger("hbg_save_status", [{"outcome": outcome.new, "target": "local", "final":true}]);
             $('#graph_storage').html("local");
             $('#graph_title').html(opt.proposed_name);
             g_aux.name = opt.proposed_name;
@@ -40,16 +40,16 @@
         }
     }
     else {
-        $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "target": "local", "final":true}]);
+        $(document).trigger("hbg_save_status", [{"outcome": outcome.no_storage, "target": "local", "final":true}]);
     }
     
     if (navigator.online) {
         if (online_service) {
             //post to service
-            $(document).trigger("hbg_save_status", [{"outcome": outcome[4], "target": "online", "final":false}]);
+            $(document).trigger("hbg_save_status", [{"outcome": outcome.pending, "target": "online", "final":false}]);
         }
         else {
-            $(document).trigger("hbg_save_status", [{"outcome": outcome[0], "target": "online", "final":true}]);
+            $(document).trigger("hbg_save_status", [{"outcome": outcome.no_storage, "target": "online", "final":true}]);
         }
     }
   };
